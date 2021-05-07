457cdbf7d374b22aa3792aa919766a6db0faff93
Kane <kgin888@gmail.com>
2021-05-07 15:25:24 +0100

    added up method

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,9 +1,21 @@
 describe ('Thermostat', function() { 
-let thermostat
-beforeEach(function() {
-  thermostat = new Thermostat 
-});
-  it ('can start at 20 degrees', function() {
-    expect(thermostat.temperature).toEqual(20)
+  let thermostat
+  beforeEach(function() {
+    thermostat = new Thermostat 
   });
+
+  describe('constructor', function() {
+     it('can start at 20 degrees', function() {
+    expect(thermostat.temperature).toEqual(20)
+    });
+  })
+  
+  describe('up', function() {
+    it('can increment temp', function() {
+      thermostat.up(5)
+      expect(thermostat.temperature).toEqual(25)
+    })
+  })
+
+  
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,4 +2,8 @@ class Thermostat{
   constructor(temperature = 20) {
     this.temperature = temperature
   }
+
+  up(amount) {
+    this.temperature += amount
+  }
 }
