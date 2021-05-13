5c8dff0d8fe4dfdd9fdd9ace3bcba412fd4e17e6
Tiffany Vallo <tiffanyvallo@tiffanys-mbp.home>
2021-05-13 22:30:34 +0100

    ajax weather API added

--- interface.js
+++ interface.js
@@ -33,4 +33,25 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector('#power-save-status').innerText = 'off';
     updateTemperature();
   })
+
+  
+    
+
+  document.querySelector('#current-city').addEventListener('change', (event) => {
+    event.preventDefault();
+    const city = event.target.value;
+
+    async function displayWeather(city) {    
+      let weatherResponse = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric`)
+      let post = await weatherResponse.json()
+      document.querySelector('#current-temp').innerText = `The current weather in ${city} is: ${capitalize(post.weather[0].description)}, ${Math.round(post.main.temp)}°C`
+    }
+  displayWeather(city);
+  })
+  
+  function capitalize(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  }
+
+  
 });
